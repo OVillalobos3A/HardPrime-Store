c736bfe2ebe94b11623fc5b999fd4ccbde51ecc3
OVillalobos3A <20190474@ricaldone.edu.sv>
2021-03-12 20:57:21 -0600

    Creación de la carpeta del proyecto.
    Se ha añadido la plantilla de html correspondiente al sitio privado, al igual que la creación del index (Login sitio privado) y pantalla de bienvenida.

---
+++ HardPrimeStore/app/controllers/dashboard/index.js
@@ -0,0 +1,3 @@
+//De momento para los elementos que corresponden al sitio privado, 
+//se ha utilizado el método M.AutoInit(); para inicializar
+//cada elemento de materialize.
---
+++ HardPrimeStore/app/helpers/dashboard_page1.php
@@ -0,0 +1,66 @@
+<?php
+//Clase para definir las plantillas de las páginas web del sitio privado
+class Dashboard_Page {
+    //Método para imprimir el encabezado y establecer el titulo del documento
+    public static function headerTemplate($title) {
+        print('
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <!--Se establece la codificación de caracteres para el documento-->
+                <meta charset="utf-8">
+                <!--Se importa la fuente de iconos de Google-->
+                <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+                <!--Se importan los archivos CSS-->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+                <link type="text/css" rel="stylesheet" href="../../resources/css/dashboard_styles.css" />
+                <!--Se informa al navegador que el sitio web está optimizado para dispositivos móviles-->
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <!--Título del documento-->
+                <title>Dashboard - '.$title.'</title>
+            </head>
+            
+            <body>
+                <!--Encabezado del documento-->
+                <header>
+                    <nav>
+                        <div class="nav-wrapper light-blue darken-4">
+                            <a href="#!" class="brand-logo">HardPrimeStore</a>
+                        </div>
+                    </nav>
+                </header>
+                <br>
+                <br>
+                <br>
+                <!--Contenido principal del documento-->
+                <main>
+        ');
+    }
+
+    //Método para imprimir el pie y establecer el controlador del documento
+    public static function footerTemplate($controller) {
+        print('
+                </main>
+                <!--Pie del documento-->
+                <footer class="page-footer light-blue darken-4">
+                    <div class="row">
+                        <div class="col s6">
+                            HardPrime Store - 2021 todos los derechos reservados
+                        </div>
+                        <div class="col s6">
+                            <a class="grey-text text-lighten-4 right" href="#!">¿Necesita ayuda?</a>
+                         </div>
+                    </div>
+                </footer>
+                <!--Importación de archivos JavaScript al final del cuerpo para una carga optimizada-->
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+                <script src="../../app/controllers/dashboard/'.$controller.'"></script>
+                <script>
+                    M.AutoInit();
+                </script>
+            </body>
+            </html>
+        ');
+    }
+}
+?>
