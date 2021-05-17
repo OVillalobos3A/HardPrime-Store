3b80657ab278a7dc07b2566da67dbec384087820
OVillalobos3A <20190474@ricaldone.edu.sv>
2021-05-17 00:36:38 -0600

    Creación del archivo de pedidos correspondiente en models.

--- HardPrimeStore/app/models/pedidos.php
+++ HardPrimeStore/app/models/pedidos.php
@@ -1,81 +1,19 @@
 <?php
 /*
-*	Clase para manejar las tablas pedidos y detalle_pedido de la base de datos. Es clase hija de Validator.
+*	Clase para manejar la tabla categorias de la base de datos. Es clase hija de Validator.
 */
 class Pedidos extends Validator
 {
     // Declaración de atributos (propiedades).
-    private $id_pedido = null;
-    private $id_detalle = null;
-    private $cliente = null;
-    private $producto = null;
-    private $cantidad = null;
-    private $precio = null;
-    private $estado = null; // Valor por defecto en la base de datos: 0
-    /*
-    *   ESTADOS PARA UN PEDIDO
-    *   0: Pendiente. Es cuando el pedido esta en proceso por parte del cliente y se puede modificar el detalle.
-    *   1: Finalizado. Es cuando el cliente finaliza el pedido y ya no es posible modificar el detalle.
-    *   2: Entregado. Es cuando la tienda ha entregado el pedido al cliente.
-    */
-
+    private $id = null;
+    private $estado = null;
     /*
     *   Métodos para asignar valores a los atributos.
     */
-    public function setIdPedido($value)
+    public function setId($value)
     {
         if ($this->validateNaturalNumber($value)) {
-            $this->id_pedido = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setIdDetalle($value)
-    {
-        if ($this->validateNaturalNumber($value)) {
-            $this->id_detalle = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setCliente($value)
-    {
-        if ($this->validateNaturalNumber($value)) {
-            $this->cliente = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setProducto($value)
-    {
-        if ($this->validateNaturalNumber($value)) {
-            $this->producto = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setCantidad($value)
-    {
-        if ($this->validateNaturalNumber($value)) {
-            $this->cantidad = $value;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setPrecio($value)
-    {
-        if ($this->validateMoney($value)) {
-            $this->precio = $value;
+            $this->id = $value;
             return true;
         } else {
             return false;
@@ -84,95 +22,85 @@ class Pedidos extends Validator
 
     public function setEstado($value)
     {
-        if ($this->validateNaturalNumber($value)) {
-            $this->estado = $value;
-            return true;
-        } else {
-            return false;
-        }
+        $this->estado = $value;
+        return true;
     }
 
     /*
     *   Métodos para obtener valores de los atributos.
     */
-    public function getIdPedido()
+    public function getId()
     {
-        return $this->id_pedido;
+        return $this->id;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
     }
 
     /*
     *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
     */
-    // Método para verificar si existe un pedido pendiente del cliente para seguir comprando, de lo contrario crea uno.
-    public function readOrder()
+    public function searchRows($value)
     {
-        $sql = 'SELECT id_pedido
-                FROM pedidos
-                WHERE estado_pedido = 0 AND id_cliente = ?';
-        $params = array($this->cliente);
-        if ($data = Database::getRow($sql, $params)) {
-            $this->id_pedido = $data['id_pedido'];
-            return true;
-        } else {
-            $sql = 'INSERT INTO pedidos(id_cliente)
-                    VALUES(?)';
-            $params = array($this->cliente);
-            // Se obtiene el ultimo valor insertado en la llave primaria de la tabla pedidos.
-            if ($this->id_pedido = Database::getLastRow($sql, $params)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
-
-    // Método para agregar un producto al carrito de compras.
-    public function createDetail()
-    {
-        $sql = 'INSERT INTO detalle_pedido(id_producto, precio_producto, cantidad_producto, id_pedido)
-                VALUES(?, ?, ?, ?)';
-        $params = array($this->producto, $this->precio, $this->cantidad, $this->id_pedido);
-        return Database::executeRow($sql, $params);
-    }
-
-    // Método para obtener los productos que se encuentran en el carrito de compras.
-    public function readCart()
-    {
-        $sql = 'SELECT id_detalle, nombre_producto, detalle_pedido.precio_producto, detalle_pedido.cantidad_producto
-                FROM pedidos INNER JOIN detalle_pedido USING(id_pedido) INNER JOIN productos USING(id_producto)
-                WHERE id_pedido = ?';
-        $params = array($this->id_pedido);
+        $sql = 'SELECT id_pedido, pedido.estado as estado, fecha_envio, fecha_pedido, clientes.nombre as cliente, empleados.nombre as encargado
+                FROM pedido INNER JOIN clientes ON pedido.id_cliente = clientes.id_cliente
+                INNER JOIN empleados ON pedido.id_empleado = empleados.id_empleado
+                WHERE clientes.nombre ILIKE ? OR pedido.estado ILIKE ?
+                ORDER BY clientes';
+        $params = array("%$value%", "%$value%");
         return Database::getRows($sql, $params);
     }
 
-    // Método para finalizar un pedido por parte del cliente.
-    public function finishOrder()
+    public function readAll()
     {
-        date_default_timezone_set('America/El_Salvador');
-        $date = date('Y-m-d');
-        $sql = 'UPDATE pedidos
-                SET estado_pedido = ?, fecha_pedido = ?
+        $sql = 'SELECT id_pedido, pedido.estado as estado, fecha_envio, fecha_pedido, clientes.nombre as cliente, empleados.nombre as encargado
+                FROM pedido INNER JOIN clientes ON pedido.id_cliente = clientes.id_cliente
+                INNER JOIN empleados ON pedido.id_empleado = empleados.id_empleado
+                ORDER BY cliente';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    public function viewShop()
+    {
+        $sql = 'SELECT  productos.nombre as nombre, cantidad, precio_producto as precio, (precio_producto*cantidad) as subtotal
+                FROM detalle_pedido INNER JOIN pedido ON detalle_pedido.id_pedido = pedido.id_pedido
+                INNER JOIN productos ON detalle_pedido.id_producto = productos.id_producto
+                WHERE detalle_pedido.id_pedido = ?';
+        $params = array($this->id);
+        return Database::getRows($sql, $params);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_pedido, pedido.estado as estado, fecha_envio, fecha_pedido, clientes.nombre as cliente, empleados.nombre as encargado
+                FROM pedido INNER JOIN clientes ON pedido.id_cliente = clientes.id_cliente
+                INNER JOIN empleados ON pedido.id_empleado = empleados.id_empleado
                 WHERE id_pedido = ?';
-        $params = array(1, $date, $this->id_pedido);
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function readOne1()
+    {
+        $this->estado1 = "Finalizado";
+        $sql = 'SELECT id_pedido, estado
+                FROM pedido 
+                WHERE id_pedido = ? and estado = ?';
+        $params = array($this->id, $this->estado1);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow()
+    {
+        $this->estado = "Finalizado";
+        $sql = 'UPDATE pedido
+                SET estado = ?
+                WHERE id_pedido = ?';
+        $params = array($this->estado, $this->id);
         return Database::executeRow($sql, $params);
     }
 
-    // Método para actualizar la cantidad de un producto agregado al carrito de compras.
-    public function updateDetail()
-    {
-        $sql = 'UPDATE detalle_pedido
-                SET cantidad_producto = ?
-                WHERE id_pedido = ? AND id_detalle = ?';
-        $params = array($this->cantidad, $this->id_pedido, $this->id_detalle);
-        return Database::executeRow($sql, $params);
-    }
-
-    // Método para eliminar un producto que se encuentra en el carrito de compras.
-    public function deleteDetail()
-    {
-        $sql = 'DELETE FROM detalle_pedido
-                WHERE id_pedido = ? AND id_detalle = ?';
-        $params = array($this->id_pedido, $this->id_detalle);
-        return Database::executeRow($sql, $params);
-    }
 }
