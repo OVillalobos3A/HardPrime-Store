b1c71e375bb766e3bb03f2886020e74c4ceff094
OVillalobos3A <20190474@ricaldone.edu.sv>
2021-06-05 00:25:18 -0600

    Implementación  de las funciones y métodos necesarios para el carrito de compras,

--- HardPrimeStore/app/controllers/public/carrito_compras.js
+++ HardPrimeStore/app/controllers/public/carrito_compras.js
@@ -35,4 +35,189 @@ document.addEventListener('DOMContentLoaded', function() {
     let instancia_dropwdown = M.Dropdown.init(dropdowns, {
     hover:false});
   });
+
+// Constante para establecer la ruta y parámetros de comunicación con la API.
+const API_CARRITO = '../../app/api/public/carrito.php?action=';
+
+// Método manejador de eventos que se ejecuta cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', function () {
+    // Se llama a la función que obtiene los productos del carrito de compras para llenar la tabla en la vista.
+    readOrderDetail();
+});
+
+// Función para obtener el detalle del pedido (carrito de compras).
+function readOrderDetail() {
+    fetch(API_CARRITO + 'readOrderDetail', {
+        method: 'get'
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                if (response.status) {
+                    // Se declara e inicializa una variable para concatenar las filas de la tabla en la vista.
+                    let content = '';
+                    let subtotal = 0;
+                    // Se declara e inicializa una variable para ir sumando cada subtotal y obtener el monto final a pagar.
+                    let total = 0;
+                    // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
+                    response.dataset.map(function (row) {
+                        subtotal = row.cost * row.cantidad;
+                        total += subtotal;
+                        // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+                        content += `
+                            <tr>
+                                <td><img src="../../resources/img/productos/${row.imagen}" class="materialboxed" height="100"></td>
+                                <td>${row.nombre}</td>
+                                <td>${row.precio}</td>
+                                <td>${row.cantidad}</td>
+                                <td>${row.subtotal}</td>
+                                <td>
+                                    <a onclick="openUpdateDialog(${row.id_detalle}, ${row.cantidad}, ${row.id_producto})" class="btn-floating btn-small btn tooltipped waves-effect waves-light orange darken-4" data-position="right" data-tooltip="modificar cantidad"><i class="material-icons">add</i></a>
+                                    <a onclick="openDeleteDialog(${row.id_detalle})" class="btn-floating btn-small btn tooltipped waves-effect waves-light orange darken-4" data-position="right" data-tooltip="remover producto"><i class="material-icons">remove</i></a>
+                                </td>
+                            </tr>
+                        `;
+                    });
+                    // Se agregan las filas al cuerpo de la tabla mediante su id para mostrar los registros.
+                    document.getElementById('tabla-carrito').innerHTML = content;
+                    document.getElementById('total').textContent = total.toFixed(2);
+                    // Se inicializa el componente Tooltip asignado a los enlaces para que funcionen las sugerencias textuales.
+                    M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+                } else {
+                    sweetAlert(4, response.exception, 'index.php');
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// Función para abrir una caja de dialogo (modal) con el formulario de cambiar cantidad de producto.
+function openUpdateDialog(id, quantity, product) {
+    // Se abre la caja de dialogo (modal) que contiene el formulario.
+    let instance = M.Modal.getInstance(document.getElementById('item-modal'));
+    instance.open();
+    // Se inicializan los campos del formulario con los datos del registro seleccionado.
+    document.getElementById('id_detalle').value = id;
+    document.getElementById('cantidad_producto').value = quantity;
+    document.getElementById('id_producto').value = product;
+    // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
+    M.updateTextFields();
+}
+
+// Método manejador de eventos que se ejecuta cuando se envía el formulario de cambiar cantidad de producto.
+document.getElementById('item-form').addEventListener('submit', function (event) {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+
+    fetch(API_CARRITO + 'updateDetail', {
+        method: 'post',
+        body: new FormData(document.getElementById('item-form'))
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                if (response.status) {
+                    // Se actualiza la tabla en la vista para mostrar el cambio de la cantidad de producto.
+                    readOrderDetail();
+                    // Se cierra la caja de dialogo (modal) del formulario.
+                    let instance = M.Modal.getInstance(document.getElementById('item-modal'));
+                    instance.close();
+                    sweetAlert(1, response.message, null);
+                } else {
+                    sweetAlert(2, response.exception, null);
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+});
+
+// Función para mostrar un mensaje de confirmación al momento de eliminar un producto del carrito.
+function openDeleteDialog(id) {
+    swal({
+        title: 'Advertencia',
+        text: '¿Está seguro de remover el producto?',
+        icon: 'warning',
+        buttons: ['No', 'Sí'],
+        closeOnClickOutside: false,
+        closeOnEsc: false
+    }).then(function (value) {
+        // Se verifica si fue cliqueado el botón Sí para realizar la petición respectiva, de lo contrario no se hace nada.
+        if (value) {
+            // Se define un objeto con los datos del registro seleccionado.
+            const data = new FormData();
+            data.append('id_detalle', id);
+
+            fetch(API_CARRITO + 'deleteDetail', {
+                method: 'post',
+                body: data
+            }).then(function (request) {
+                // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+                if (request.ok) {
+                    request.json().then(function (response) {
+                        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                        if (response.status) {
+                            // Se cargan nuevamente las filas en la tabla de la vista después de borrar un producto del carrito.
+                            readOrderDetail();
+                            sweetAlert(1, response.message, null);
+                        } else {
+                            sweetAlert(2, response.exception, null);
+                        }
+                    });
+                } else {
+                    console.log(request.status + ' ' + request.statusText);
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
+    });
+}
+
+// Función para mostrar un mensaje de confirmación al momento de finalizar el pedido.
+function finishOrder() {
+    swal({
+        title: 'Aviso',
+        text: '¿Está seguro de finalizar el pedido?',
+        icon: 'info',
+        buttons: ['No', 'Sí'],
+        closeOnClickOutside: false,
+        closeOnEsc: false
+    }).then(function (value) {
+        // Se verifica si fue cliqueado el botón Sí para realizar la petición respectiva, de lo contrario se muestra un mensaje.
+        if (value) {
+            fetch(API_CARRITO + 'finishOrder', {
+                method: 'get'
+            }).then(function (request) {
+                // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+                if (request.ok) {
+                    request.json().then(function (response) {
+                        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                        if (response.status) {
+                            sweetAlert(1, response.message, 'index.php');
+                        } else {
+                            sweetAlert(2, response.exception, null);
+                        }
+                    });
+                } else {
+                    console.log(request.status + ' ' + request.statusText);
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        } else {
+            sweetAlert(4, 'Puede seguir comprando', null);
+        }
+    });
+}
+
   
---
+++ HardPrimeStore/app/models/carrito.php
@@ -0,0 +1,204 @@
+<?php
+/*
+*	Clase para manejar las tablas pedidos y detalle_pedido de la base de datos. Es clase hija de Validator.
+*/
+class Pedidos extends Validator
+{
+    // Declaración de atributos (propiedades).
+    private $id_pedido = null;
+    private $id_detalle = null;
+    private $cliente = null;
+    private $producto = null;
+    private $cantidad = null;
+    private $precio = null;
+    private $estado = null; // Valor por defecto en la base de datos: 0
+    /*
+    *   ESTADOS PARA UN PEDIDO
+    *   "En preparacion". Es cuando el pedido esta en proceso por parte del cliente y se puede modificar el detalle.
+    *   "Finalizado". Es cuando el cliente finaliza el pedido y ya no es posible modificar el detalle.
+    *   "Entregado". Es cuando la tienda ha entregado el pedido al cliente.
+    *   "Cancelado". Es cuando el cliente se arrepiente de haber realizado el pedido.
+    */
+
+    /*
+    *   Métodos para validar y asignar valores de los atributos.
+    */
+    public function setIdPedido($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_pedido = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setIdDetalle($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_detalle = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCliente($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->cliente = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setProducto($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->producto = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCantidad($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->cantidad = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setPrecio($value)
+    {
+        if ($this->validateMoney($value)) {
+            $this->precio = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setEstado($value)
+    {
+        $this->estado = $value;
+        return true;
+    }
+
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getIdPedido()
+    {
+        return $this->id_pedido;
+    }
+
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+    */
+    // Método para verificar si existe un pedido en proceso para seguir comprando, de lo contrario se crea uno.
+    public function startOrder()
+    {
+        $this->estado = "En preparacion";
+
+        $sql = 'SELECT id_pedido
+                FROM pedido
+                WHERE estado = ? AND id_cliente = ?';
+        $params = array($this->estado, $_SESSION['id_cliente']);
+        if ($data = Database::getRow($sql, $params)) {
+            $this->id_pedido = $data['id_pedido'];
+            return true;
+        } else {
+            $sql = 'INSERT INTO pedido(estado, id_cliente)
+                    VALUES(?, ?)';
+            $params = array($this->estado, $_SESSION['id_cliente']);
+            // Se obtiene el ultimo valor insertado en la llave primaria de la tabla pedidos.
+            if ($this->id_pedido = Database::getLastRow($sql, $params)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // Método para agregar un producto al carrito de compras.
+    public function createDetail()
+    {
+        // Se realiza una subconsulta para obtener el precio del producto.
+        $sql = 'INSERT INTO detalle_pedido(id_producto, precio_producto, cantidad, id_pedido)
+                VALUES(?, (SELECT precio FROM productos WHERE id_producto = ?), ?, ?)';
+        $params = array($this->producto, $this->producto, $this->cantidad, $this->id_pedido);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para obtener los productos que se encuentran en el carrito de compras.
+    public function readOrderDetail()
+    {
+        $sql = "SELECT id_detalle, nombre, detalle_pedido.cantidad, ('$' || ' ' || CAST(detalle_pedido.precio_producto AS varchar)) as precio, 
+                ('$' || ' ' || CAST((detalle_pedido.precio_producto*detalle_pedido.cantidad) AS varchar)) as subtotal, imagen, id_producto, detalle_pedido.precio_producto as cost
+                FROM pedido INNER JOIN detalle_pedido USING(id_pedido) INNER JOIN productos USING(id_producto)
+                WHERE id_pedido = ?";
+        $params = array($this->id_pedido);
+        return Database::getRows($sql, $params);
+    }
+
+    // Método para finalizar un pedido por parte del cliente.
+    public function finishOrder()
+    {
+        // Se establece la zona horaria local para obtener la fecha del servidor.
+        date_default_timezone_set('America/El_Salvador');
+        $date = date('Y-m-d');
+        $this->estado = "Finalizado";
+        $sql = 'UPDATE pedido
+                SET estado = ?, fecha_pedido = ?
+                WHERE id_pedido = ?';
+        $params = array($this->estado, $date, $_SESSION['id_pedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para actualizar la cantidad de un producto agregado al carrito de compras.
+    public function updateDetail()
+    {
+        $sql = 'UPDATE detalle_pedido
+                SET cantidad = ?
+                WHERE id_detalle = ? AND id_pedido = ?';
+        $params = array($this->cantidad, $this->id_detalle, $_SESSION['id_pedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    // Método para eliminar un producto que se encuentra en el carrito de compras.
+    public function deleteDetail()
+    {
+        $sql = 'DELETE FROM detalle_pedido
+                WHERE id_detalle = ? AND id_pedido = ?';
+        $params = array($this->id_detalle, $_SESSION['id_pedido']);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function verifyCantidad()
+    {
+        $sql = "SELECT nombre, stock 
+                FROM productos 
+                WHERE id_producto = ? and stock >= ?";
+        $params = array($this->producto,$this->cantidad);
+        return Database::getRows($sql, $params);
+    }
+
+    public function verifyProduct()
+    {
+        $sql = "SELECT id_producto
+                FROM pedido INNER JOIN detalle_pedido USING(id_pedido) INNER JOIN productos USING(id_producto)
+                WHERE id_detalle = ?";
+        $params = array($this->id_detalle);
+        if ($data = Database::getRow($sql, $params)) {
+            $this->producto = $data['id_producto'];
+        } else {
+        }
+    }  
+
+
+}
